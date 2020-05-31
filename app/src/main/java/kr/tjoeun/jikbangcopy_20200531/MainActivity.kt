95d8a965d7a65889be37d59bf048c10cdd5440f1
janggalman <janggalman@gmail.com>
2020-05-31 12:12:34 +0900

    메인 액티비티 BaseActivity로 상속

--- app/src/main/java/kr/tjoeun/jikbangcopy_20200531/MainActivity.kt
+++ app/src/main/java/kr/tjoeun/jikbangcopy_20200531/MainActivity.kt
@@ -2,11 +2,22 @@ package kr.tjoeun.jikbangcopy_20200531
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.tjoeun.jikbangcopy_20200531.datas.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
     }
 }
