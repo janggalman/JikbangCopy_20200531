1aa06554ea80ada71c17187fcdd6541ee2f453d8
janggalman <janggalman@gmail.com>
2020-05-31 12:39:25 +0900

    방 데이터 ArrayList 추가

--- app/src/main/java/kr/tjoeun/jikbangcopy_20200531/MainActivity.kt
+++ app/src/main/java/kr/tjoeun/jikbangcopy_20200531/MainActivity.kt
@@ -2,9 +2,15 @@ package kr.tjoeun.jikbangcopy_20200531
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.tjoeun.jikbangcopy_20200531.adapters.RoomAdapter
 import kr.tjoeun.jikbangcopy_20200531.datas.BaseActivity
+import kr.tjoeun.jikbangcopy_20200531.datas.Room
 
 class MainActivity : BaseActivity() {
+
+    val rooms = ArrayList<Room>()
+
+    lateinit var roomAdapter:RoomAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +25,16 @@ class MainActivity : BaseActivity() {
 
     override fun setValues() {
 
+        rooms.add(Room(8000,"마포구 서교동",1,"마포 서교동의 방입니다."))
+        rooms.add(Room(35000,"마포구 서교동",3,"마포 서교동의 방입니다."))
+        rooms.add(Room(17000,"마포구 서교동",5,"마포 서교동의 방입니다."))
+        rooms.add(Room(5000,"마포구 서교동",4,"마포 서교동의 방입니다."))
+        rooms.add(Room(12000,"마포구 성산동",0,"마포 성산동의 방입니다."))
+        rooms.add(Room(45000,"마포구 성산동",-1,"마포 성산동의 방입니다."))
+        rooms.add(Room(18000,"마포구 성산동",-2,"마포 성산동의 방입니다."))
+        rooms.add(Room(56000,"마포구 망원동",2,"마포 망원동의 방입니다."))
+        rooms.add(Room(82000,"마포구 망원동",0,"마포 망원동의 방입니다."))
+        rooms.add(Room(4000,"마포구 망원동",1,"마포 망원동의 방입니다."))
     }
+
 }
--- app/src/main/java/kr/tjoeun/jikbangcopy_20200531/adapters/RoomAdapter.kt
+++ app/src/main/java/kr/tjoeun/jikbangcopy_20200531/adapters/RoomAdapter.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
+import kr.tjoeun.jikbangcopy_20200531.R
 import kr.tjoeun.jikbangcopy_20200531.datas.Room
 
 class RoomAdapter(context: Context, resId:Int , list:List<Room>) : ArrayAdapter<Room>(context,resId,list) {
@@ -14,7 +16,22 @@ class RoomAdapter(context: Context, resId:Int , list:List<Room>) : ArrayAdapter<
     val inf = LayoutInflater.from (mContext)
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        return super.getView(position, convertView, parent)
+
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = inf.inflate(R.layout.room_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        val data = mList.get(position)
+
+        val priceTxt = row.findViewById<TextView>(R.id.priceTxt)
+        val addressTxt = row.findViewById<TextView>(R.id.addressTxt)
+        val memoTxt = row.findViewById<TextView>(R.id.memoTxt)
+
+        return row
+
     }
 
 
